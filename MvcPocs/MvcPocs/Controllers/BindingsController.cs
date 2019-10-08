5e2edb7a2770d99a8285b49c4f273af7cb166ad5
Gerardo G <g.garciac@outlook.com>
2019-10-08 16:12:00 -0500

    Demo para ver un flujo sencillo de Binding de modelo de la vista y validación del modelo de la vista

---
+++ MvcPocs/MvcPocs/Controllers/BindingsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MvcPocs.Models;
+
+namespace MvcPocs.Controllers
+{
+    public class BindingsController : Controller
+    {
+        public static List<BindingsDemoVm> Data = new List<BindingsDemoVm>();
+        public IActionResult Index()
+        {
+            return View(Data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(BindingsDemoVm model)
+        {
+            if (UnaReglaDeNegocioNoSeCumple(model))
+            {
+                ModelState.AddModelError("RN-001", "No se cumplió la regla xxxxxx");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            try
+            {
+                Data.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("EX-001", "Un mensaje error que no revele información sensible, pero que esté de acuerdo a la excepción que ocurrió.");
+                return View();
+            }
+        }
+
+        private bool UnaReglaDeNegocioNoSeCumple(BindingsDemoVm model)
+        {
+            return model.DayOfWeek == Models.DayOfWeek.Sunday && model.Name == "Polo";
+        }
+    }
+}
---
+++ MvcPocs/MvcPocs/Models/BindingsDemoVm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPocs.Models
+{
+    public enum DayOfWeek
+    {
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    }
+    public class BindingsDemoVm
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(minimum: 100, maximum: 200, ErrorMessage = "El valor indicado no está en el rango permitido")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "El día de la semana debe ser indicado")]
+        [DisplayName("Day of Week")]
+        public DayOfWeek DayOfWeek { get; set; }
+
+        [Required]
+        public DateTime Day { get; set; }
+    }
+}
